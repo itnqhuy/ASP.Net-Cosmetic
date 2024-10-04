2e5822abd417aa1d1898278d5a070a14d7d2da3b
itnqhuy <huyahhin2019@gmail.com>
2024-10-04 18:29:33 +0700

    Update cac chuyen trang tu trang chu

---
+++ CosmeticMVC/Controllers/PostsController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using CosmeticMVC.Data;
+
+namespace CosmeticMVC.Controllers
+{
+    public class PostsController : Controller
+    {
+        private readonly CosmeticContext _context;
+
+        public PostsController(CosmeticContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Posts
+        public async Task<IActionResult> Index()
+        {
+            var cosmeticContext = _context.Posts.Include(p => p.IdProductNavigation);
+            return View(await cosmeticContext.ToListAsync());
+        }
+
+        // GET: Posts/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var post = await _context.Posts
+                .Include(p => p.IdProductNavigation)
+                .FirstOrDefaultAsync(m => m.IdPost == id);
+            if (post == null)
+            {
+                return NotFound();
+            }
+
+            return View(post);
+        }
+
+        // GET: Posts/Create
+        public IActionResult Create()
+        {
+            ViewData["IdProduct"] = new SelectList(_context.Products, "IdProduct", "IdProduct");
+            return View();
+        }
+
+        // POST: Posts/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("IdPost,Content,Description,Hide,Thumbail,Meta,ModifiedAt,Datebegin,Order,IdProduct")] Post post)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(post);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IdProduct"] = new SelectList(_context.Products, "IdProduct", "IdProduct", post.IdProduct);
+            return View(post);
+        }
+
+        // GET: Posts/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var post = await _context.Posts.FindAsync(id);
+            if (post == null)
+            {
+                return NotFound();
+            }
+            ViewData["IdProduct"] = new SelectList(_context.Products, "IdProduct", "IdProduct", post.IdProduct);
+            return View(post);
+        }
+
+        // POST: Posts/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("IdPost,Content,Description,Hide,Thumbail,Meta,ModifiedAt,Datebegin,Order,IdProduct")] Post post)
+        {
+            if (id != post.IdPost)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(post);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PostExists(post.IdPost))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IdProduct"] = new SelectList(_context.Products, "IdProduct", "IdProduct", post.IdProduct);
+            return View(post);
+        }
+
+        // GET: Posts/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var post = await _context.Posts
+                .Include(p => p.IdProductNavigation)
+                .FirstOrDefaultAsync(m => m.IdPost == id);
+            if (post == null)
+            {
+                return NotFound();
+            }
+
+            return View(post);
+        }
+
+        // POST: Posts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var post = await _context.Posts.FindAsync(id);
+            if (post != null)
+            {
+                _context.Posts.Remove(post);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PostExists(string id)
+        {
+            return _context.Posts.Any(e => e.IdPost == id);
+        }
+    }
+}
---
+++ CosmeticMVC/ViewComponents/ListPostViewComponent.cs
@@ -0,0 +1,35 @@
+﻿using CosmeticMVC.Data;
+using CosmeticMVC.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CosmeticMVC.ViewComponents
+{
+    public class ListPostViewComponent: ViewComponent
+    {
+        private readonly CosmeticContext db;
+
+        public ListPostViewComponent(CosmeticContext context) => db = context;
+
+        public IViewComponentResult Invoke()
+        {
+            var data = (from p in db.Posts
+                join prod in db.Products on p.IdProduct equals prod.IdProduct
+                select new ListPostVM()
+                {
+                    Id_post = p.IdPost,
+                    Content = p.Content,
+                    Description = p.Description,
+                    Hide = p.Hide,
+                    Thumbail = p.Thumbail,
+                    Meta = p.Meta,
+                    Modified_at = p.ModifiedAt,
+                    Datebegin = p.Datebegin,
+                    Order = p.Order,
+                    Name_product =prod.Name
+
+                }).ToList();
+
+            return View(data);
+        }
+    }
+}
