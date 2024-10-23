be46249c7cdc0dc2304c3fc012474842189bfceb
itnqhuy <huyahhin2019@gmail.com>
2024-10-23 16:49:23 +0700

    add favourite panel

---
+++ CosmeticMVC/Controllers/FavouriteController.cs
@@ -0,0 +1,67 @@
+﻿using CosmeticMVC.Data;
+using CosmeticMVC.Helpers;
+using CosmeticMVC.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CosmeticMVC.Controllers
+{
+    public class FavouriteController : Controller
+    {
+        private readonly CosmeticContext db;
+
+        public FavouriteController(CosmeticContext context)
+        {
+            db = context;
+        }
+
+        public List<ListFavoriteVM> Favourite => HttpContext.Session.Get<List<ListFavoriteVM>>(MySetting.FAVOURITE_KEY) ?? new List<ListFavoriteVM>();
+
+        public IActionResult Index()
+        {
+            return View(Favourite);
+        }
+
+        public IActionResult AddToCartItem(string id, int quantity = 1)
+        {
+            var yeuThich = Favourite;
+            var item = yeuThich.SingleOrDefault(p => p.IdProduct == id);
+            if (item == null)
+            {
+                var hangHoa = db.Products.Include(p => p.IdImageNavigation).SingleOrDefault(p => p.IdProduct == id);
+                if (hangHoa == null)
+                {
+                    TempData["Message"] = $"Không tìm thấy hàng hóa có mã {id}";
+                    return Redirect("/404");
+                }
+
+                item = new ListFavoriteVM()
+                {
+                    IdProduct = id,
+                    ProductImage =
+                        $"{hangHoa.IdImageNavigation.Name.ToString()}.{hangHoa.IdImageNavigation.Type.ToString()}",
+                    ProductName = hangHoa.Name,
+                    Price = hangHoa.Price,
+                };
+                yeuThich.Add(item);
+            }
+
+            HttpContext.Session.Set(MySetting.FAVOURITE_KEY, yeuThich);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult RemoveFromCartItem(string id)
+        {
+            var yeuThich = Favourite;
+            var item = yeuThich.SingleOrDefault(p => p.IdProduct == id);
+            if (item != null)
+            {
+                yeuThich.Remove(item);
+                HttpContext.Session.Set(MySetting.FAVOURITE_KEY, yeuThich);
+            }
+
+            return RedirectToAction("Index");
+
+        }
+    }
+}
--- CosmeticMVC/Helpers/MySetting.cs
+++ CosmeticMVC/Helpers/MySetting.cs
@@ -3,5 +3,6 @@
     public class MySetting
     {
         public static string CART_KEY = "MYCART";
+        public static string FAVOURITE_KEY = "MYFAVOURITE";
     }
 }
